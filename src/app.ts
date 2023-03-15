f0960de0153d0054f297ee302b85feb02933a857
Vitaly Tsaplin <tsaplin@adobe.com>
2023-03-16 00:23:53 +0100

    feat: deploy to ethos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import { App } from '@slack/bolt';
 
-const CHANNEL_ID = process.env.SLACK_DEFAULT_CHANNEL as string;
+const SERVER_PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3030;
+const SLACK_DEFAULT_CHANNEL = process.env.SLACK_DEFAULT_CHANNEL as string;
+
+console.log('SLACK_DEFAULT_CHANNEL: ', SLACK_DEFAULT_CHANNEL);
+console.log(`SLACK_BOT_TOKEN: ${process.env.SLACK_BOT_TOKEN}`);
+console.log(`SLACK_APP_TOKEN: ${process.env.SLACK_APP_TOKEN}\n`)
 
 type Message = {
   id: string
@@ -16,11 +21,11 @@ const app = new App({
 });
 
 (async () => {
-  const wss = new WebSocketServer({ port: 3030 });
+  const wss = new WebSocketServer({ port: SERVER_PORT });
   wss.on('connection', function connection(ws) {
 
     app.client.chat.postMessage({
-      channel: CHANNEL_ID,
+      channel: SLACK_DEFAULT_CHANNEL,
       text: 'New client connected'
     });
 
@@ -31,7 +36,7 @@ const app = new App({
         const payload = JSON.parse(data.toString());
         console.log('text: ', payload.text);
         app.client.chat.postMessage({
-          channel: CHANNEL_ID,
+          channel: SLACK_DEFAULT_CHANNEL,
           text: payload.text,
           username: payload.name,
         });
@@ -57,7 +62,7 @@ const app = new App({
         text: messageEvent.text,
       }
       console.log('messageEvent.text: ', messageEvent.text);
-      if (messageEvent.channel === CHANNEL_ID) {
+      if (messageEvent.channel === SLACK_DEFAULT_CHANNEL) {
         console.log('sending to clients');
         wss.clients.forEach(function each(client) {
           console.log(`client.readyState: ${client.readyState}`);
