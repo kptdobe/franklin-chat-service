07faf295d20b6bf25c39c0cce7173945d94b8664
Alexandre Capt <acapt@adobe.com>
2023-03-16 09:18:54 +0100

    feat: admin channel for admin messages

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import { WebSocket, WebSocketServer } from 'ws';
 import { App } from '@slack/bolt';
 
 const SERVER_PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3030;
-const SLACK_DEFAULT_CHANNEL = process.env.SLACK_DEFAULT_CHANNEL as string;
 
-console.log('SLACK_DEFAULT_CHANNEL: ', SLACK_DEFAULT_CHANNEL);
-console.log(`SLACK_BOT_TOKEN: ${process.env.SLACK_BOT_TOKEN}`);
-console.log(`SLACK_APP_TOKEN: ${process.env.SLACK_APP_TOKEN}\n`)
+const SLACK_ADMIN_CHANNEL_ID = process.env.SLACK_ADMIN_CHANNEL_ID as string;
+const SLACK_DEFAULT_CHANNEL_ID = process.env.SLACK_DEFAULT_CHANNEL_ID as string;
 
 type Message = {
   id: string
@@ -25,7 +23,7 @@ const app = new App({
   wss.on('connection', function connection(ws) {
 
     app.client.chat.postMessage({
-      channel: SLACK_DEFAULT_CHANNEL,
+      channel: SLACK_ADMIN_CHANNEL_ID,
       text: 'New client connected'
     });
 
@@ -36,7 +34,7 @@ const app = new App({
         const payload = JSON.parse(data.toString());
         console.log('text: ', payload.text);
         app.client.chat.postMessage({
-          channel: SLACK_DEFAULT_CHANNEL,
+          channel: SLACK_DEFAULT_CHANNEL_ID,
           text: payload.text,
           username: payload.name,
         });
@@ -82,7 +80,7 @@ const app = new App({
         text: messageEvent.text,
       }
       console.log('messageEvent.text: ', messageEvent.text);
-      if (messageEvent.channel === SLACK_DEFAULT_CHANNEL) {
+      if (messageEvent.channel === SLACK_DEFAULT_CHANNEL_ID) {
         console.log('sending to clients');
         wss.clients.forEach(function each(client) {
           console.log(`client.readyState: ${client.readyState}`);
