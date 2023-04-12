7b0826c4b2b835c8786e4f564dab5959c32d8a99
Vitaly Tsaplin <vitaly@tsaplin.com>
2023-04-12 12:50:28 +0200

    feat: ssl and refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import { WebSocket, WebSocketServer } from 'ws';
-import { App } from '@slack/bolt';
+import { WebSocket, WebSocketServer, RawData } from 'ws';
+import { App as Slack } from '@slack/bolt';
 
 const SERVER_PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 8081;
 
@@ -12,44 +12,44 @@ type Message = {
   text: string
 }
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-});
-
-(async () => {
-  const wss = new WebSocketServer({ port: SERVER_PORT });
-  wss.on('connection', function connection(ws) {
-
-    console.log('New client connected');
-    app.client.chat.postMessage({
+function handleConnection(wss: WebSocketServer, slack: Slack) {
+  return function connection(ws: WebSocket) {
+    console.log('client connected');
+    slack.client.chat.postMessage({
       channel: SLACK_ADMIN_CHANNEL_ID,
       text: 'New client connected'
     });
-
+    ws.on('message', handleChatMessage(wss, slack));
     ws.on('error', console.error);
-    ws.on('message', function message(data) {
-      console.log('received: %s', data);
-      if (data) {
-        const payload = JSON.parse(data.toString());
-        console.log('text: ', payload.text);
-        app.client.chat.postMessage({
-          channel: SLACK_DEFAULT_CHANNEL_ID,
-          text: payload.text,
-          username: payload.name,
-        });
-        wss.clients.forEach(function each(client) {
-          if (client.readyState === WebSocket.OPEN) {
-            console.log(`sending to client: ${data}`);
-            client.send(JSON.stringify(payload));
-          }
-        });
-      }
+    ws.on('close', () => {
+      console.log('client disconnected');
     });
-  });
+  }
+}
 
-  app.event('message', async ({ event, say }) => {
+function handleChatMessage(wss: WebSocketServer, slack: Slack) {
+  return function (data: RawData) {
+    console.log('received: %s', data);
+    if (data) {
+      const payload = JSON.parse(data.toString());
+      console.log('text: ', payload.text);
+      slack.client.chat.postMessage({
+        channel: SLACK_DEFAULT_CHANNEL_ID,
+        text: payload.text,
+        username: payload.name,
+      });
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          console.log(`sending to client: ${data}`);
+          client.send(JSON.stringify(payload));
+        }
+      });
+    }
+  }
+}
+
+function handleSlackMessage(wss: WebSocketServer, slack: Slack) {
+  return async function ({ event, say }: any) {
     console.log('received message: ', event);
     if (!event.subtype) {
       const messageEvent = event as any;
@@ -61,15 +61,15 @@ const app = new App({
 
       if (messageEvent.user) {
         // eslint-disable-next-line no-await-in-loop
-        const res = await app.client.users.info({
+        const res = await slack.client.users.info({
           user: messageEvent.user,
         });
 
         if (res) {
           name = res.user?.profile?.real_name as string;
-            // email: res.user.profile.email,
-            // avatar: res.user.profile.image_72,
-            // source: 'slack',
+          // email: res.user.profile.email,
+          // avatar: res.user.profile.image_72,
+          // source: 'slack',
         }
       } else if (messageEvent.user_profile && messageEvent.user_profile.real_name) {
         name = messageEvent.user_profile.real_name;
@@ -94,8 +94,22 @@ const app = new App({
     } else {
       console.log(event.subtype);
     }
+  };
+}
+
+(async () => {
+  const wss = new WebSocketServer({ port: SERVER_PORT });
+
+  const slack = new Slack({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
   });
-  await app.start();
+
+  wss.on('connection', handleConnection(wss, slack));
+  slack.event('message', handleSlackMessage(wss, slack));
+
+  await slack.start();
 
   console.log(`\nAdmin channel: ${SLACK_ADMIN_CHANNEL_ID}`);
   console.log(`Default channel: ${SLACK_DEFAULT_CHANNEL_ID}\n`);
