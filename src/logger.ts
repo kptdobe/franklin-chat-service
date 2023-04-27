fd2d4d2ff0529d9c6056a393fc5e3f870a6515e4
Vitaly Tsaplin <tsaplin@adobe.com>
2023-04-27 11:25:01 +0200

    chore: spelling

--- src/logger.ts
+++ src/logger.ts
@@ -4,13 +4,13 @@ import LokiTransport from 'winston-loki';
 const GRAFANA_HOST = process.env.GRAFANA_HOST as string;
 const GRAFANA_APP_NAME = process.env.GRAFANA_APP_NAME as string;
 
-const GRAFANA_USERID = process.env.GRAFANA_USER_ID as string;
+const GRAFANA_USER_ID = process.env.GRAFANA_USER_ID as string;
 const GRAFANA_PASSWORD = process.env.GRAFANA_PASSWORD as string;
 
 console.log('\nGrafana Logger');
 console.log(`Grafana Host: ${GRAFANA_HOST}`);
 console.log(`Grafana App Name: ${GRAFANA_APP_NAME}`);
-console.log(`Grafana User ID: ${GRAFANA_USERID}\n`);
+console.log(`Grafana User ID: ${GRAFANA_USER_ID}\n`);
 
 export const logger = winston.createLogger({
     level: 'debug',
@@ -19,7 +19,7 @@ export const logger = winston.createLogger({
             host: GRAFANA_HOST,
             labels: { app: GRAFANA_APP_NAME },
             json: true,
-            basicAuth: GRAFANA_USERID + ":" + GRAFANA_PASSWORD,
+            basicAuth: GRAFANA_USER_ID + ":" + GRAFANA_PASSWORD,
             format: winston.format.json(),
             replaceTimestamp: true,
             onConnectionError: (err) => console.error(err)
